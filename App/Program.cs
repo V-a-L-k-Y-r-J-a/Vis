9963b66cc92222fc1aadf284354cbbaa3c516ec1
V-a-L-k-Y-r-J-a <abramenko.vlada@mail.ru>
2023-10-11 13:21:49 +0700

    01

---
+++ App/Program.cs
@@ -0,0 +1,63 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            double firstValue, secondValue;
+            string action;
+
+            try 
+            {
+                Console.WriteLine("Введите число_1");
+                firstValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите число_2");
+                secondValue = double.Parse(Console.ReadLine());
+            }
+            catch(Exception ) 
+            {
+                Console.WriteLine("не удалось преобразовать строки ");
+                Console.ReadLine();
+                continue;
+            }
+            
+            Console.WriteLine("Выбор операции '+' '-' '*' '/'");
+            action = Console.ReadLine();
+
+            if (action == "+")
+            {
+                Console.WriteLine(firstValue + secondValue);
+            }
+            else if (action == "-")
+            {
+                Console.WriteLine(firstValue - secondValue);
+            }
+            else if (action == "*")
+            {
+                Console.WriteLine(firstValue * secondValue);
+            }
+            else if (action == "/")
+            {
+                if (secondValue == 0)
+                {
+
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(firstValue / secondValue);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
